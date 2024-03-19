3714a05babfded8bf93fa5cf24591a939a048c23
João Souza <ms.joao.felipe@gmail.com>
2024-03-19 14:24:44 -0300

    fix: adding 0.0.0.0 due to connection refused

--- app.py
+++ app.py
@@ -14,13 +14,15 @@ class User(Resource):
     def post(self):
         return {'message': 'teste'}
 
+
+class UserDetail(Resource):
     def get(self, cpf):
         return {'message': 'CPF'}
 
 
 api.add_resource(Users, '/users')
 api.add_resource(User, '/user')
-api.add_resource(User, '/user/', '/user/<string:cpf>')
+api.add_resource(UserDetail, '/user/<string:cpf>')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
