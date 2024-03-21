127a36d26773e3d58794fc2078a47ae9f339b657
João Souza <ms.joao.felipe@gmail.com>
2024-03-21 16:14:18 -0300

    test: testing post user

--- tests/test_application.py
+++ tests/test_application.py
@@ -9,6 +9,35 @@ class TestApplication:
         app = create_app('config.MockConfig')
         return app.test_client()
 
+    @pytest.fixture
+    def valid_user(self):
+        return {
+            "first_name": "Felipe",
+            "last_name": "Mendes",
+            "cpf": "501.933.250-04",
+            "email": "felipe@gmail.com",
+            "birth_date": "1999-04-06"
+        }
+
+    @pytest.fixture
+    def invalid_user(self):
+        return {
+            "first_name": "Felipe",
+            "last_name": "Mendes",
+            "cpf": "501.933.250-05",
+            "email": "felipe@gmail.com",
+            "birth_date": "1999-04-06"
+        }
+
     def test_get_users(self, client):
         response = client.get('/users')
         assert response.status_code == 200
+
+    def test_post_user(self, client, valid_user, invalid_user):
+        response = client.post('/user', json=valid_user)
+        assert response.status_code == 200
+        assert b"successfully" in response.data
+
+        response = client.post('/user', json=invalid_user)
+        assert response.status_code == 400
+        assert b"invalid" in response.data
